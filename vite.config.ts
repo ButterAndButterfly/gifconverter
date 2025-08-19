d99f9e1abecf9712eae09293af675a944a0aba5e
二土 <lijia0732@sina.com>
2025-08-19 19:19:33 +0800

    feat: change base `/gifconverter/` to `/`

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
-    base: "/gifconverter/",
+    base: "/",
     server: {
         headers: {
             "Cross-Origin-Opener-Policy": "same-origin",
